bb22dad767011f4d3c0f6a993da38b00af84af48
coca create <himotoki.sansu@gmail.com>
2024-12-21 18:23:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ pip install --upgrade pip setuptools wheel
 # PyTorchのインストール（CUDA対応）
 pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cpu
 pip install spacy
-python -m spacy download en_core_web_lg
+python -m spacy download en_core_web_sm
